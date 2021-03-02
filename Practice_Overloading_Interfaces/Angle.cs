58268f94b20b4652bb56702557c5e6dccc89235a
baloxegal <baloxegal@gmail.com>
2021-03-02 19:11:28 +0200

    Add project files.

---
+++ Practice_Overloading_Interfaces/Angle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Overloading_Interfaces
+{
+    class Angle
+    {
+        public int Degrees
+        {
+            get;
+            set;
+        }
+        public int Minutes
+        {
+            get;
+            set;
+        }
+        public int Seconds
+        {
+            get;
+            set;
+        }
+        public static Angle operator + (Angle a, Angle b)
+        {
+            var d = 0;
+            var m = 0;
+            var s = a.Seconds + b.Seconds;
+            if (s >= 60)
+            {
+                m++;
+                s -= 60;
+            }
+            m += a.Minutes + b.Minutes;
+            if(m >= 60)
+            {
+                d++;
+                m -= 60;
+            }
+            d += a.Degrees + b.Degrees;
+            return new Angle{Degrees = d, Minutes = m, Seconds = s};            
+        }
+        public static Angle operator +(Angle a, int angle)
+        {
+            return new Angle { Degrees = a.Degrees + angle, Minutes = a.Minutes, Seconds = a.Seconds };
+        }        
+        public static Angle operator -(Angle a, Angle b)
+        {
+            var d = 0;
+            var m = 0;
+            var s = a.Seconds - b.Seconds;
+            if (s < 0)
+            {
+                m--;
+                s += 60;
+            }
+            m = a.Minutes - b.Minutes + m;
+            if (m < 0)
+            {
+                d--;
+                m += 60;
+            }
+            d = a.Degrees - b.Degrees + d;
+            return new Angle { Degrees = d, Minutes = m, Seconds = s };
+        }
+        public static Angle operator -(Angle a, int angle)
+        {
+            return new Angle { Degrees = a.Degrees - angle, Minutes = a.Minutes, Seconds = a.Seconds };
+        }
+        public static Angle operator /(Angle a, int b)
+        {
+            int s = (a.Degrees * 60 + a.Minutes * 60 + a.Seconds * 60) / b;
+            int d = s / 60;
+            int m = (int)(s % 60 * 0.6 / 60);
+            s = s % 60;
+            if(d > 1)
+            {
+
+            }
+            var m = 0;
+            
+            if (s < 0)
+            {
+                m--;
+                s += 60;
+            }
+            m = a.Minutes - b.Minutes + m;
+            if (m < 0)
+            {
+                d--;
+                m += 60;
+            }
+            d = a.Degrees - b.Degrees + d;
+            return new Angle { Degrees = d, Minutes = m, Seconds = s };
+        }
+    }
+}
---
+++ Practice_Overloading_Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Overloading_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle a = new Angle{ Degrees = 42, Minutes = 16, Seconds = 34 };
+            Angle b = new Angle { Degrees = 56, Minutes = 22, Seconds = 59 };
+            Angle c = a - b;
+            Console.WriteLine($"Angle is: {c.Degrees}, {c.Minutes}, {c.Seconds}");
+
+            Angle d = new Angle { Degrees = 48, Minutes = 41, Seconds = 54 };
+            Angle e = new Angle { Degrees = 126, Minutes = 32, Seconds = 31 };
+            Angle f = d + e;
+            Console.WriteLine($"Angle is: {f.Degrees}, {f.Minutes}, {f.Seconds}");
+        }
+    }
+}
